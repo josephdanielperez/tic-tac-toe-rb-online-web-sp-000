8e1bd30235d89ba26a3dc546e4498d0449df6cf8
Joseph Perez <hello@josephdanielperez.com>
2020-06-15 00:18:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,9 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " # | # | # "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " # | # | # "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " # | # | # "
 end
 
 def input_to_index(user_input)
@@ -14,18 +14,18 @@ def player_move(board, index, marker)
  board[index] = marker
 end
 
-def move(board, index, current_player)
-  board[index] = current_player
-end
-
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
 end
 
 def valid_move?(board, index)
   if !position_taken?(board, index) && (index).between?(0,8)
     return true
-  else
+  else 
     return false
   end
 end
@@ -50,21 +50,21 @@ def turn_count(board)
   counter = 0
   board.each {|space|
     if space == "X" || space == "O"
-      counter += 1
+      counter += 1   
     end
   }
   counter
 end
 
 WIN_COMBINATIONS = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[6,4,2]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ]
 
 def won?(board)
@@ -72,11 +72,11 @@ def won?(board)
     win_index_1 = single_win_combo[0]
     win_index_2 = single_win_combo[1]
     win_index_3 = single_win_combo[2]
-
+    
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-
+   
     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
       return single_win_combo
     end
@@ -103,7 +103,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if draw?(board) || won?(board) || full?(board)
+  if draw?(board) || won?(board) || full?(board) 
     return true
   else
     return false
@@ -121,5 +121,17 @@ def play(board)
   until counter == 9
   turn(board)
   counter += 1
+  end
+end
+
+def play(board)
+  until over?(board)
+    turn(board) 
+  end
+  if won?(board)
+    winner(board) == "X" || winner(board) == "O"
+    puts "Congratulations #{winner(board)}!"
+  else draw?(board)
+    puts "Cat\'s Game!"
   end
 end
